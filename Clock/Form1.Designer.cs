098be92e07ab896cb0eae4c5c656f6e503ea6597
phamtrang <phamtrang19981998@gmail.com>
2019-09-25 08:24:06 +0700

    first commit

---
+++ Clock/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(32, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(455, 385);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.PictureBox1_LoadCompleted);
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 416);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 449);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Display Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Clock/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+        Timer t = new Timer();
+        int W = 300, H = 300, s = 140, m = 130, h = 120;// chiều dài đồng hồ, và chiều dài các kim
+
+        int cx, cy;// tọa độ tâm đồng hồ
+        Bitmap bmp;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(W + 1, H + 1);
+            cx = H / 2;
+            cy = W / 2;
+            t.Interval = 1000;
+            t.Tick += new EventHandler(this.t_Tick);
+            t.Start();
+            BackColor = Color.White;
+
+        }
+        private void t_Tick(object sender, EventArgs e)
+        {
+            
+            g = Graphics.FromImage(bmp);
+
+            // lấy thời gian
+            int ss = DateTime.Now.Second;
+            int mm = DateTime.Now.Minute;
+            int hh = DateTime.Now.Hour;
+
+            int[] handCoord = new int[2];
+
+            //clear
+            g.Clear(Color.White);
+
+            //vẽ hình tròn
+            g.DrawEllipse(new Pen(Color.Black, 1f), 0, 0, W, H);
+
+            //vẽ số giờ
+            g.DrawString("12", new Font("Arial", 12), Brushes.Black, new PointF(140, 2));
+            g.DrawString("3", new Font("Arial",12), Brushes.Black, new PointF(286, 140));
+            g.DrawString("6", new Font("Arial", 12), Brushes.Black, new PointF(142, 282));
+            g.DrawString("9", new Font("Arial",12), Brushes.Black, new PointF(0,140));
+
+            //kim giây
+            handCoord = msCoord(ss, s);
+            g.DrawLine(new Pen(Color.Red, 1f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            //kim phút
+            handCoord = msCoord(mm, m);
+            g.DrawLine(new Pen(Color.Green, 2f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            //kim giờ
+            handCoord = hrCoord(hh % 12, mm, h);
+            g.DrawLine(new Pen(Color.Black, 3f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            //đưa bmp vào pictureBox;
+            pictureBox1.Image = bmp;
+
+            // hiển thị thời gian
+         
+            DateTime now = DateTime.Now;
+            string a = now.ToString("hh");
+            string b = now.ToString("mm");
+            string c = now.ToString("ss");
+
+            label1.Text = "Hour :" + a + " Minute :" + b + " Second :" + c;
+
+            //  dispose
+            g.Dispose();
+
+
+
+
+        }
+        // hàm xử lí kim phut và kim giây
+        private int[] msCoord(int val, int hlen)
+        {
+            //hlen là chiều dài kim phut hoặc giây
+            int[] coord = new int[2];
+            val *= 6;// cứ mỗi phut hoặc 1 giây sẽ tăng 360/60=6 độ;
+
+            if (val >= 0 && val < 180) 
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+
+           
+        }
+        //hàm xử lí chuyển động kim  giờ
+        private int[] hrCoord(int hval,int mval, int hlen)
+        {
+            //hlen là chiêu dài kim giờ
+            //hval là số giờ, mval là số phút
+            int[] coord = new int[2];
+            //mỗi giờ sẽ tăng thêm 30 độ=360/12;
+            //mỗi phút sẽ tăng thêm 0.5 độ=30/60;
+
+            int val = (int)((hval * 30) + (mval * 0.5));
+
+            if (val >= 0 && val < 180)
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));// 
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+
+
+        }
+
+    }
+}
